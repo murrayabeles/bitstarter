6f1170b45749b5af75ec60b83d00225af2066d6f
Murray Abeles <murray@murrayabeles.com>
2013-07-11 04:26:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+fs.readFile('index.html', 'utf8', function(err, data) {
 app.get('/', function(request, response) {
-  		response.send('data');
-	});
-
+  		response.send(data);
+});
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
